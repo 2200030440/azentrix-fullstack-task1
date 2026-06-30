e074f64b31efeeb765a3b613928b0171472ad149
Ananya Sinha <2200033092cser@gmail.com>
2026-06-30 20:01:49 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? '/azentrix-fullstack-task1/' : './',
   server: {
     port: 3000,
     open: true
